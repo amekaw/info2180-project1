5fec5f2ffed1c5f21d33a2921e2d490cb5820959
Ameka Williams <amekaw8@gmail.com>
2022-10-29 12:10:55 -0500

    Added initial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
 /* Add your JavaScript to this file */
+window.onload = function()
+{
+    var msg = document.getElementsbyClassName("message");
+    var buttn = document.querySelector("button");
+    var eml = document.getElementbyId("email").value;
+    buttn.addEventListener("click", function(event) 
+    {
+        event.preventDefault();
+        if (eml == "") {
+            msg[0].innerHTML = "Please enter a valid email address";    
+    } 
+    else {
+        msg[0].innerHTML = "Thank you! Your email address " + eml + " has been added to our mailing list!";
+    }
+    });
+}
